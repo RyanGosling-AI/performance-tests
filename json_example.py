fb3b992649a2e30f90fe2da428ea171af71e51c1
RyanGosling-AI <damnedwretchofficial@gmail.com>
2026-09-19 14:55:14 +1000

    Введение в HTTPX

---
+++ json_example.py
@@ -0,0 +1,20 @@
+import json
+
+json_data = '{"name": "Иван", "age": 30, "is_student": false}'
+parsed_data = json.loads(json_data)
+print(parsed_data["name"])
+
+data = {
+    "name": "Мария",
+    "age": 25,
+    "is_student": True
+}
+json_string = json.dumps(data, indent=4, ensure_ascii=False)
+print(json_string)
+
+with open("json_example.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+    print(data)
+
+with open("data.json", "w", encoding="utf-8") as file:
+    json.dump(data, file, indent=4, ensure_ascii=False)
